1133fcfa66af9acb14fac09f070cf2aeef18150a
CrisRod <crodriguez85@gmail.com>
2019-07-02 10:15:49 -0400

    Seteo de app.js y primeros componentes

---
+++ frontend/src/components/ItemDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemDetail extends Component {
+    render() {
+      return (
+        <div className="container">
+          <h1>Detalle de Items</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default ItemDetail;
---
+++ frontend/src/components/ItemList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemList extends Component {
+    render() {
+      return (
+        <div className="container">
+          <h1>Lista de Items</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default ItemList;
