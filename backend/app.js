43bf011c6d3b4f6124a60168dda9abad55bcac0b
CrisRod <crodriguez85@gmail.com>
2019-07-02 08:11:24 -0400

    categories items backend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const port = 8000;
 
 const items = require('./routes/items');
-const  categorias = require('./routes/categories');
+// const  categorias = require('./routes/categories');
 
 const app = express();
 
@@ -14,4 +15,30 @@ app.use((req, res, next) => {
     if(allowedDomains.indexOf(origin) > -1) {
         res.setHeader("Access-Control-Allow-Origin", origin);
     }
-})
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+      );
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: false }));
+app.use(cookieParser());
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/", items);
+// app.use("/api/categories", categorias);
+
+app.listen(port, () => {
+  console.log(`server listening on localhost:${port}`)
+});
+
+
+module.exports = app;
+
