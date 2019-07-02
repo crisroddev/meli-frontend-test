0515c3d9f0c24b265a0bd16c39259e49e458b662
CrisRod <crodriguez85@gmail.com>
2019-07-02 11:37:53 -0400

    actionTypes redux

---
+++ frontend/src/redux/actionTypes.js
@@ -0,0 +1,7 @@
+
+export const SEARCH_FIELD = "SEARCH_FIELD";
+export const ITEMS = "ITEMS";
+export const ITEM = "ITEM";
+export const CLEAR_SEARCH = "CLEAR_SEARCH";
+export const CLEAR_ITEMS = "CLEAR_ITEMS";
+export const CATEGORIES = "CATEGORIES";
